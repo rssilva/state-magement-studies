c49470eace2619ab86d7b899ae8ffc3123272d55
Rafael Specht <rafaelspdasilva@gmail.com>
2022-01-10 15:33:52 -0300

    well, redux still working fine

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { useFetch } from '../hooks/useFetch.ts'
+import React from 'react'
+// import { useFetchFiles } from '../hooks/useFetchFiles.ts'
 
 const Item = ({ file, onChange }) => {
-  const fileName = file
-    .replace('_discography.json', '')
-    .replace('_', ' ')
+  const fileName = file.replace('_discography.json', '').replace('_', ' ')
 
   return (
-    <li>
+    <li key={file}>
       <input id={file} onClick={(ev) => onChange(ev)} type="checkbox" />
-      <label id={file}>{fileName}</label>
+      <label htmlFor={file}>{fileName}</label>
     </li>
   )
 }
 
-export const List = ({ items, onChange }) => {
+type Args = {
+  items: { [key: string]: unknown }[]
+  onChange: (file: string, ev: Event) => void
+}
+
+export const List = ({ items, onChange }: Args): React.ReactElement => {
   // const files: string[] = useFetch()
 
-  function onItemClick (ev, file) {
+  function onItemClick(ev, file) {
     onChange(file, ev.target.checked)
   }
 
   return (
     <ul>
-      {items.map((file) => <Item key={file} file={file} onChange={(ev) => onItemClick(ev, file)} />)}
+      {items.map((file) => (
+        <Item key={file} file={file} onChange={(ev) => onItemClick(ev, file)} />
+      ))}
     </ul>
   )
 }
--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,5 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addUser, removeUser } from '../store/users.ts'
+
+function UserCard({ user, onClickRemove }) {
+  return (
+    <div>
+      <span>{user.name}</span>
+      <button onClick={() => onClickRemove(user.id)}>x</button>
+    </div>
+  )
+}
 
 export function TasksPage(): React.ReactElement {
-  return <div>tasks...</div>
+  const dispatch = useDispatch()
+  const [value, setValue] = useState('')
+
+  const users = useSelector((state) => {
+    return state.users.users
+  })
+
+  function onRemove(id) {
+    dispatch(removeUser(id))
+  }
+
+  return (
+    <div>
+      <div>
+        <input onKeyUp={(ev) => setValue(ev.target.value)} />
+        <button onClick={() => dispatch(addUser(value))}>Add</button>
+      </div>
+      <div>
+        {users.map((user) => {
+          return (
+            <UserCard
+              key={user.id}
+              user={user}
+              onClickRemove={(id) => onRemove(id)}
+            />
+          )
+        })}
+        {/* <button onClick={() => dispatch(addUser())}>alow</button>
+        <button onClick={() => dispatch(decrement())}>down</button> */}
+      </div>
+    </div>
+  )
 }
